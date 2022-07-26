77ce18a595c39a594a46625eb62a15ec941dbda7
KBDss <josren45@kingshigh.school.nz>
2022-07-26 14:55:53 +1200

    finished
    finished one of the videos on traps

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -1,12 +1,13 @@
 extends Camera2D
 
-var target = null
+var target 
 var zoomed = false
 var center = Vector2.ZERO
 
 
 func _ready():
 	center = get_viewport_rect().size/2
+
 	
 func _process(delta):
 	if Input.is_action_just_pressed("zoom"):
@@ -14,13 +15,13 @@ func _process(delta):
 			target = null
 			zoomed = false
 		else:
-			target = owner.get_node("Player").global_position
+			target = get_parent().get_node("Player")
 			zoomed = true
 			
 			
 	if zoomed:
-		zoom = zoom.move_toward(Vector2(0.3,0.3), 0.03)
-		position = position.move_toward(target, 80)
+		zoom = Vector2(0.3,0.3)
+		position = target.global_position
 	else:
-		zoom = zoom.move_toward(Vector2(1,1), 0.03)
-		position = position.move_toward(center,80)
+		zoom = Vector2(1,1)
+		position = center
