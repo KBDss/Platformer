f768ac052e680f4bb2519aa43e2be867c873116e
KBDss <josren45@kingshigh.school.nz>
2022-08-15 09:45:26 +1200

    SKOSDJa
    dsadasd

---
+++ Scenes/SpinningBlade.gd
@@ -0,0 +1,11 @@
+extends Node2D
+export (int) var follow_speed = 50
+
+func _ready():
+	$AnimationPlayer.play("Spin")
+
+
+func_process(delta):
+	$Path2D/PathFollow2D.offset += follow_speed * delta
+
+
